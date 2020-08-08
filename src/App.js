c1c94512978966f9af90b5e8257734d0f9c428f9
cjayprime <nnachijioke@yahoo.com>
2020-08-08 14:54:39 +0100

    Adds sections to home pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Routes from './routes';
 
-import Configuration from './mamaket.config.js';
-
 // TODO: Install 'notistack'
 const App = () => (
   <Routes/>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from 'react';
-import { Paper, Grid, Box, InputBase, IconButton, Container } from '@material-ui/core';
+import { Paper, Grid, Box, InputBase, IconButton, Container, GridList, GridListTile, GridListTileBar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Search } from '@material-ui/icons';
+import { Cake, ChevronRight, Search, Room } from '@material-ui/icons';
 
 import Header from '../components/Header';
 
 import ladyInYellow from '../assets/images/home/ladyInYellow.png';
 import guyOnYellowBackground from '../assets/images/home/guyOnYellowBackground.png';
 import twoGuysOnWhiteBackground from '../assets/images/home/twoGuysOnWhiteBackground.png';
+import appStore from '../assets/images/home/appStore.png';
+import footer from '../assets/images/logo/footer.png';
+import meatAndPoultry from '../assets/images/category/meatAndPoultry.png';
+import drink from '../assets/images/category/drink.png';
+import frozenChicken from '../assets/images/category/frozenChicken.png';
+import naijaIngredients from '../assets/images/category/naijaIngredients.png';
+import freshFruits from '../assets/images/category/freshFruits.png';
 
 const usePaperStyles = makeStyles(() => ({
   root: {
@@ -35,13 +42,67 @@ const useInputStyles = makeStyles(() => ({
     fontSize: 15
   },
 }));
+const useGridListTileBarStyles = makeStyles(() => ({
+  root: {
+    backgroundColor: '#FFFFFF'
+  },
+  titleWrap: {
+    color: '#BBBCC1'
+  },
+  title: {
+    color: '#BBBCC1',
+    fontSize: 12,
+    marginBottom: 5
+  },
+  subtitle: {
+    color: '#BBBCC1',
+    fontSize: 15
+  }
+}));
+const categories = [
+  {title: 'Meat & Poultry', image: meatAndPoultry},
+  {title: 'Drinks', image: drink},
+  {title: 'Frozen Foods', image: frozenChicken},
+  {title: 'Naija Ingredients', image: naijaIngredients},
+  {title: 'Beauty & Skincare', image: naijaIngredients},
+  {title: 'Household items', image: naijaIngredients},
+  {title: 'Fashion', image: naijaIngredients},
+  {title: 'Vegetables', image: naijaIngredients},
+  {title: 'Fresh Fruits', image: freshFruits},
+  {title: 'Snacks', image: naijaIngredients},
+  {title: 'Baby Products', image: naijaIngredients},
+  {title: 'Health & wellness', image: naijaIngredients}
+];
+const products = [
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+  {title: 'Fresh apples ready for delivery', price: '18,000', location: 'Ajah, Lekki', image: meatAndPoultry},
+];
 
 const Home = () => {
   const paperClasses = usePaperStyles();
   const inputClasses = useInputStyles();
+  const gridListTileBarClasses = useGridListTileBarStyles();
   
   useEffect(() => {
-    document.scrollingElement.scrollTop = document.scrollingElement.scrollHeight
+    document.scrollingElement.scrollTop = 0.45 * document.scrollingElement.scrollHeight
   });
 
   return (
@@ -60,45 +121,36 @@ const Home = () => {
         </Box>
       </Paper>
 
-      <Container maxWidth="lg" style={{backgroundColor: 'grey'}}>
+      <Container maxWidth="lg">
         <Grid container spacing={0}>
-          <Grid item xs={4} style={{width: '100%', height: 300, width: 'auto', backgroundColor: '#FEB300', backgroundPosition: '60% -60%', overflow: 'hidden', backgroundImage: "url('" + guyOnYellowBackground + "')"}}>
+          <Grid item xs={3} style={{height: 300, width: 'auto', backgroundColor: '#FEB300', backgroundPosition: '60% -60%', overflow: 'hidden', backgroundImage: "url('" + guyOnYellowBackground + "')"}}>
             <div style={{width: 90, float: 'right', fontSize: 30, fontFamily: 'BebasNeue', marginRight: '15%', lineHeight: '25px', color: '#F8F8F8', marginTop: 30}}>
               BECOME A SELLER
               <br/>
               <div style={{width: 155, fontSize: 13, fontFamily: 'Quicksand', lineHeight: '15px', color: '#F8F8F8'}}>Sell anything, anytime, anyday</div>
             </div>
           </Grid>
-          <Grid item xs={8} style={{height: 300, width: 'auto', backgroundColor: '#F1EAE0', overflow: 'hidden', backgroundImage: "url('" + twoGuysOnWhiteBackground + "')", backgroundPosition: '30% 5px'}}>
+          <Grid item xs={9} style={{height: 300, width: 'auto', backgroundColor: '#F1EAE0', overflow: 'hidden', backgroundImage: "url('" + twoGuysOnWhiteBackground + "')", backgroundPosition: '30% 5px'}}>
             <div style={{fontSize: 50, fontFamily: 'BebasNeue', color: '#AFA294', width: 200, lineHeight: '45px', marginTop: 60, marginLeft: 50}}>SHOPPING MADE EASY</div>
             <div style={{fontSize: 20, fontFamily: 'Quicksand', color: '#AFA294', width: 250, marginTop: 0, marginLeft: 50}}>Make orders from your convenience</div>
           </Grid>
         </Grid>
 
+
         <Grid container spacing={0}>
-          <Grid item xs={4}>
+          <Grid item xs={3}>
             <Grid item xs={12} style={{backgroundColor: '#0177B6', height: 70, padding: 20, color: '#FFFFFF', textAlign: 'center', fontSize: 20}}>
               All Categories
             </Grid>
             <Grid item xs={12} style={{backgroundColor: '#FFF', border: '1px solid #0177B6'}}>
               {
-                [
-                  {title: 'Meat & Poultry'},
-                  {title: 'Drinks'},
-                  {title: 'Frozen Foods'},
-                  {title: 'Naija Ingredients'},
-                  {title: 'Beauty & Skincare'},
-                  {title: 'Household items'},
-                  {title: 'Fashion'},
-                  {title: 'Vegetables'},
-                  {title: 'Fresh Fruits'},
-                  {title: 'Snacks'},
-                  {title: 'Baby Products'},
-                  {title: 'Health & wellness'}
-                ]
-                .map((prop, i) => (
-                  <Paper key={i} style={{fontSize: 13, borderRadius: 0, padding: 12.5, marginLeft: 10, marginRight: 10, borderBottom: '1px solid #eee', boxShadow: '0 0 0 0', cursor: 'pointer'}}>
-                    {prop.title}
+                categories.map((prop, i) => (
+                  <Paper key={i} style={{fontSize: 13, borderRadius: 0, padding: 12.5, marginLeft: 10, marginRight: 10, borderBottom: '1px solid #eee', boxShadow: '0 0 0 0', cursor: 'pointer', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                      <Cake />
+                      <span style={{marginLeft: 5}}>{prop.title}</span>
+                    </div>
+                    <ChevronRight />
                   </Paper>
                 ))
               }
@@ -106,24 +158,90 @@ const Home = () => {
                 See more
               </Paper>
             </Grid>
-          </Grid>
-          
-          <Grid item xs={12}>
-            <Grid item xs={12} style={{backgroundColor: '#0177B6', height: 70, padding: 20, color: '#FFFFFF', textAlign: 'center', fontSize: 20}}>
-              Product Categories
-            </Grid>
+            <Paper style={{boxShadow: '0 0 0 0', marginTop: 10, padding: 5, borderRadius: 0, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', fontFamily: 'Quicksand', fontSize: 20, fontWeight: 'bold', textAlign: 'center'}}>
+              <div style={{width: '60%', color: '#0177B6'}}>
+                Download the Mamaket mobile
+              </div>
+              <div style={{color: '#FFAF00', marginBottom: 15}}>Application</div>
+              <img src={appStore} alt="App Store" />
+            </Paper>
           </Grid>
 
-          {/* <Grid item xs={8} style={{marginTop: 20}}>
-            <div style={{fontSize: 50, fontFamily: 'BebasNeue', color: '#AFA294', width: 200, lineHeight: '45px', marginTop: 60, marginLeft: 50}}>SHOPPING MADE EASY</div>
-            <div style={{fontSize: 20, fontFamily: 'Quicksand', color: '#AFA294', width: 200, marginTop: 0, marginLeft: 50}}>Make orders from your convenience</div>
-          </Grid> */}
+          <Grid item xs={9}>
+            <Grid item xs={12} style={{height: 50, padding: 10, fontSize: 20, color: '#BBBCC1'}}>
+              Product Categories
+            </Grid>
+            <GridList cols={4} spacing={10}>
+              {
+                categories.map((props, i) => (
+                  <GridListTile key={i} cols={1} style={{height: 300}}>
+                    <img src={props.image} alt={props.title} />
+                    <GridListTileBar title={props.title} style={{position: 'absolute', top: 'calc(50% - 24px)', backgroundColor: '#FFFFFF17', border: '1px solid #0177B6', right: 'auto', left: '50%', transform: 'translate(-50%, 0)'}}/>
+                  </GridListTile>
+                ))
+              }
+            </GridList>
+          </Grid>
+        </Grid>
+
+
+        <Grid container style={{margin: 10, marginTop: 30, marginLeft: 0, fontSize: 20, color: '#BBBCC1'}}>
+          Trending Products
+        </Grid>
+        <Grid container style={{padding: 10, backgroundColor: '#DDDEE0'}}>
+            <GridList cols={5} spacing={10} cellHeight={220}>
+              {
+                products.map((props, i) => (
+                  <GridListTile key={i} cols={1}>
+                    <div style={{display: 'flex', alignItems: 'center', zIndex: 1000000, position: 'absolute', left: 10, color: '#FFF', fontSize: 12, fontWeight: 'bold'}}>
+                      <Room htmlColor='#FFF' />
+                      {props.location}
+                    </div>
+                    <img src={props.image} alt={props.title} />
+                    <GridListTileBar title={props.title} subtitle={'N '+props.price} classes={gridListTileBarClasses} />
+                  </GridListTile>
+                ))
+              }
+            </GridList>
         </Grid>
       </Container>
 
-      <Paper style={{width: '90%', marginLeft: '5%', backgroundColor: 'blue', height: 100 }}>
 
-      </Paper>
+      <Grid container alignItems="center" style={{backgroundColor: '#0177B6', fontSize: 17, fontFamily: 'Quicksand', color: '#FFFFFF', height: 500, marginTop: 50,}}>
+          <Grid item xs={3}>
+            <img src={footer} alt="App Store" />
+          </Grid>
+          <Grid container item xs={2} direction="column" justify="space-between" style={{height: 200}}>
+            <div>About us</div>
+            <div>About Mamaket</div>
+            <div>Terms & Conditions</div>
+            <div>Privacy Policy</div>
+            <div>Become a seller</div>
+          </Grid>
+          <Grid container item xs={2} direction="column" justify="space-between" style={{height: 200}}>
+            <div>Support</div>
+            <div>Email</div>
+            <div>Safety Tips</div>
+            <div>Contact Us</div>
+            <div>FAQ</div>
+          </Grid>
+          <Grid container item xs={2} direction="column" justify="space-between" style={{height: 200}}>
+            <div>Our Socials</div>
+            <div>Facebook</div>
+            <div>Instagram</div>
+            <div>Instagram</div>
+            <div>Youtube</div>
+          </Grid>
+          <Grid container item xs={3} direction="column" justify="space-between" alignItems="flex-start" style={{height: 200}}>
+            <div>Our Apps</div>
+            <div>Download now</div>
+            <img src={appStore} alt="App Store" />
+          </Grid>
+      </Grid>
+
+      {/* <Paper style={{width: '90%', marginLeft: '5%', backgroundColor: 'blue', height: 100 }}>
+
+      </Paper> */}
     </>
   );
 };
